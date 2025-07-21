b66837092c8cf9c919cee14b771cc436b596e076
iamqazi <qaziobaid7897@gmail.com>
2025-07-21 16:49:17 +0500

    first commit

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import { ReactNode } from "react";
-import classNames from "classnames";
+
 interface DiagonalButtonProps {
   children: ReactNode;
   className?: string;
@@ -11,16 +11,13 @@ interface DiagonalButtonProps {
 
 export default function DiagonalButton({
   children,
-  className,
+  className = "",
   onClick,
 }: DiagonalButtonProps) {
   return (
     <button
       onClick={onClick}
-      className={classNames(
-        "relative uppercase font-bold text-[17px] border-2 border-white px-5 py-2 text-white overflow-hidden transition-all duration-1000 hover:text-black group rounded-xl",
-        className
-      )}
+      className={`relative uppercase font-bold text-[17px] border-2 border-white px-5 py-2 text-white overflow-hidden transition-all duration-1000 hover:text-black group rounded-xl ${className}`}
     >
       <span className="absolute left-[-40px] top-0 h-full w-0 bg-white skew-x-12 transition-all duration-1000 z-[-1] group-hover:w-[160%]" />
       <span className="relative z-10">{children}</span>
